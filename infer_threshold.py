c91aaa61f3ed5af42c45aef5b32a88c6fe2e0bf9
xunih <xunih@outlook.com>
2021-05-31 21:27:00 +0200

    Remove unnecessary code

--- infer_threshold.py
+++ infer_threshold.py
@@ -4,45 +4,31 @@ from matplotlib import pyplot
 import csv
 import pandas as pd
 
+data_file = 'data.csv'
+ground_truth_file = 'classification.csv'
+
 
 def main():
-    #ground_truth_one, ground_truth_two, similarity_score_uni, similarity_score_lev, similarity_score_jac, similarity_score_pair, similarity_score_cos = get_data()
-    '''print(ground_truth_one)
-    print(ground_truth_two)
-    print(similarity_score_uni)
-    print(similarity_score_lev)
-    print(similarity_score_jac)
-    print(similarity_score_pair)
-    print(similarity_score_cos)
-
-    print('a')
+    ground_truth_one, ground_truth_two, similarity_score_uni, similarity_score_lev, similarity_score_jac, similarity_score_pair, similarity_score_cos = get_data()
     infer_threshold(ground_truth_one, similarity_score_uni)
-    print('b')
+
     infer_threshold(ground_truth_two, similarity_score_uni)
-    '''
-    ground_truth_one, ground_truth_two,similarity_score_cos = get_data()
 
-    #print('a')
-    #infer_threshold(ground_truth_one, similarity_score_lev)
-    #print('b')
-    #infer_threshold(ground_truth_two, similarity_score_lev)
+    infer_threshold(ground_truth_one, similarity_score_lev)
 
-    #print('a')
-    #infer_threshold(ground_truth_one, similarity_score_jac)
-    #print('b')
-    #infer_threshold(ground_truth_two, similarity_score_jac)
+    infer_threshold(ground_truth_two, similarity_score_lev)
 
-    '''
-    print('a')
+    infer_threshold(ground_truth_one, similarity_score_jac)
+
+    infer_threshold(ground_truth_two, similarity_score_jac)
+
     infer_threshold(ground_truth_one, similarity_score_pair)
-    print('b')
-    infer_threshold(ground_truth_two, similarity_score_pair)
-    '''
-    print('a')
-    infer_threshold(ground_truth_one, similarity_score_cos)
-    print('b')
-    infer_threshold(ground_truth_two, similarity_score_cos)
 
+    infer_threshold(ground_truth_two, similarity_score_pair)
+
+    infer_threshold(ground_truth_one, similarity_score_cos)
+
+    infer_threshold(ground_truth_two, similarity_score_cos)
 
 
 def get_data():
@@ -53,32 +39,25 @@ def get_data():
     similarity_score_jac = []
     similarity_score_pair = []
     similarity_score_cos = []  # small model
-    file = pd.read_csv('saved_file/file_with_vector_correct_column.csv')
+    file = pd.read_csv(data_file)
 
-    #similairty_universal = file['Similarity_universal']
-    #similarity_lev = file['Similarity_Levenshtein']
-    #similarity_jac = file['Similarity_Jaccard']
-    #similarity_pair = file['Similarity_Pairwise']
+    similairty_universal = file['Similarity_universal_normalized']
+    similarity_lev = file['Similarity_Levenshtein']
+    similarity_jac = file['Similarity_Jaccard']
+    similarity_pair = file['Similarity_Pairwise_normalized']
     similarity_cos = file['Similarity_cosine_large_model_nonnormalized']
-    file_class = pd.read_csv('saved_file/classification.csv')
+    file_class = pd.read_csv(ground_truth_file)
     column_classification = file_class['Classification']
-    #for row in similairty_universal:
-       # similarity_score_uni.append(row)
-    #for row in similarity_lev:
-       # similarity_score_lev.append(row)
-    '''
-        try:
-          similarity_score_lev.append(float(row))
-        except:
-            print(row)
-            print('please enter a valid number')
-        '''
+    for row in similairty_universal:
+        similarity_score_uni.append(row)
+    for row in similarity_lev:
+        similarity_score_lev.append(row)
 
-    #for row in similarity_jac:
-       # similarity_score_jac.append(row)
+    for row in similarity_jac:
+        similarity_score_jac.append(row)
 
-    # for row in similarity_pair:
-       # similarity_score_pair.append(row)
+    for row in similarity_pair:
+        similarity_score_pair.append(row)
     for row in similarity_cos:
         similarity_score_cos.append(row)
 
@@ -95,11 +74,9 @@ def get_data():
         ground_truth_list_one.append(ground_truth_one)
         ground_truth_list_two.append(ground_truth_two)
 
-    # print(similarity_score_list,ground_truth_list)
-    # return ground_truth_list_one, ground_truth_list_two, \
-           # similarity_score_uni, similarity_score_lev, \
-        #   similarity_score_jac, similarity_score_pair, similarity_score_cos
-    return ground_truth_list_one, ground_truth_list_two, similarity_score_cos
+    return ground_truth_list_one, ground_truth_list_two, \
+           similarity_score_uni, similarity_score_lev, \
+           similarity_score_jac, similarity_score_pair, similarity_score_cos
 
 
 def infer_threshold(ground_truth, similarity_score):
@@ -123,25 +100,6 @@ def infer_threshold(ground_truth, similarity_score):
     index_text = ''.join(f'{index} / {len(thresholds)}')
     print(index_text)
     print(results)
-
-
-    # # If you want to plot a precision/recall curve:
-    pyplot.plot(recalls, precisions, 'g')
-    pyplot.plot(recalls[index], precisions[index], 'ro', label=results)
-    pyplot.xlabel('Recall')
-    pyplot.ylabel('Precision')
-    pyplot.legend()
-    #pyplot.savefig('jac_a_plot1')
-    pyplot.show()
-
-    # If you want to plot a threshold/f-score curve:
-    pyplot.plot(thresholds, fscores[:-1], 'g')
-    pyplot.plot(thresholds[index], fscores[index], 'ro', label=results)
-    pyplot.xlabel('Threshold')
-    pyplot.ylabel('F-score')
-    pyplot.legend()
-    #pyplot.savefig('jac_a_plot2')
-    pyplot.show()
 
 
 if __name__ == '__main__':
